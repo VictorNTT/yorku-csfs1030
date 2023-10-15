bb6095097ecf8bb7de10cd8c937a46e5e7622fd7
Victor Nguyen <intelteq@hotmail.com>
2023-10-15 17:00:29 -0400

    upload file example

--- sql_examples/Upload_File_Example.sql
+++ sql_examples/Upload_File_Example.sql
@@ -7,9 +7,16 @@ mysql> SELECT @@GLOBAL.secure_file_priv;
 | C:\ProgramData\MySQL\MySQL Server 8.0\Uploads\ |
 +------------------------------------------------+
 
-2. Create a table
+2. Create a table UPLOAD_FILE_TEST
+
 3. Create an input file, comma-separated values UPLOAD_FILE_TEST.csv
-   and save it at the 'Uploads' folders
+   and save it at the 'Uploads' folders. The file should have two columns: ID, MSG. 
+   
+   Example   
++----------------------------------------------   
+	1, The fox jumps over the lazy dog
+	2, There are several ways to update data
++----------------------------------------------
 */
 
 
@@ -20,10 +27,13 @@ CREATE TABLE UPLOAD_FILE_TEST (
 );
 
 /*
- SELECT @@GLOBAL.secure_file_priv;  
  To find out the the full path of 'Uploads' folder
+ SELECT @@GLOBAL.secure_file_priv;  
+ 
+ Note: Use the forward slash / instead of backward slash \ in the file path 
+ 
 */
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/UPLOAD_FILE_TEST.txt' INTO TABLE UPLOAD_FILE_TEST FIELDS TERMINATED BY ',';
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/UPLOAD_FILE_TEST.csv' INTO TABLE UPLOAD_FILE_TEST FIELDS TERMINATED BY ',';
 
 SELECT * FROM UPLOAD_FILE_TEST;
 
